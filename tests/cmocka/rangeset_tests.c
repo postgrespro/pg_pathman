6d0da5ff35830b146d246587ce2b91f4434e1766
Dmitry Ivanov <ivadmi5@gmail.com>
2016-10-29 20:15:31 +0300

    fix CFLAGS in Makefile, add more tests in test_irange_list_union()

--- tests/cmocka/rangeset_tests.c
+++ tests/cmocka/rangeset_tests.c
@@ -9,19 +9,26 @@
 #include "debug_print.c"
 
 
-/* declarations of tests */
-static void test_1(void **state);
+/*
+ * -----------------------
+ *  Declarations of tests
+ * -----------------------
+ */
+
+static void test_irange_list_union(void **state);
 
 
 /* Entrypoint */
 int
 main(void)
 {
+	/* Array of test functions */
 	const struct CMUnitTest tests[] =
 	{
-		cmocka_unit_test(test_1),
+		cmocka_unit_test(test_irange_list_union),
 	};
 
+	/* Run series of tests */
 	return cmocka_run_group_tests(tests, NULL, NULL);
 }
 
@@ -32,17 +39,72 @@ main(void)
  */
 
 static void
-test_1(void **state)
+test_irange_list_union(void **state)
 {
-	IndexRange a = make_irange(0, 100, true),
-			   b = make_irange(20, 50, false);
-
+	IndexRange a, b;
 	List	  *union_result;
 
+
+	/* Subtest #0 */
+	a = make_irange(0, 100, true);
+	b = make_irange(0, 100, true);
+	union_result = irange_list_union(list_make1_irange(a),
+									 list_make1_irange(b));
+
+	assert_string_equal(rangeset_print(union_result),
+						"[0-100]L");
+
+	/* Subtest #1 */
+	a = make_irange(0, 100, true);
+	b = make_irange(0, 100, false);
+	union_result = irange_list_union(list_make1_irange(a),
+									 list_make1_irange(b));
+
+	assert_string_equal(rangeset_print(union_result),
+						"[0-100]C");
+
+	/* Subtest #2 */
+	a = make_irange(0, 100, true);
+	b = make_irange(0, 50, false);
+	union_result = irange_list_union(list_make1_irange(a),
+									 list_make1_irange(b));
+
+	assert_string_equal(rangeset_print(union_result),
+						"[0-50]C, [51-100]L");
+
+	/* Subtest #3 */
+	a = make_irange(0, 100, true);
+	b = make_irange(50, 100, false);
+	union_result = irange_list_union(list_make1_irange(a),
+									 list_make1_irange(b));
+
+	assert_string_equal(rangeset_print(union_result),
+						"[0-49]L, [50-100]C");
+
+	/* Subtest #4 */
+	a = make_irange(0, 100, true);
+	b = make_irange(50, 99, false);
+	union_result = irange_list_union(list_make1_irange(a),
+									 list_make1_irange(b));
+
+	assert_string_equal(rangeset_print(union_result),
+						"[0-49]L, [50-99]C, 100L");
+
+	/* Subtest #5 */
+	a = make_irange(0, 100, true);
+	b = make_irange(1, 100, false);
+	union_result = irange_list_union(list_make1_irange(a),
+									 list_make1_irange(b));
+
+	assert_string_equal(rangeset_print(union_result),
+						"0L, [1-100]C");
+
+	/* Subtest #6 */
+	a = make_irange(0, 100, true);
+	b = make_irange(20, 50, false);
 	union_result = irange_list_union(list_make1_irange(a),
 									 list_make1_irange(b));
 
 	assert_string_equal(rangeset_print(union_result),
 						"[0-19]L, [20-50]C, [51-100]L");
 }
-
