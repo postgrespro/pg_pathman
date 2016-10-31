0675d1165e22084ae41cb82b5891734678a19031
Maksim Milyutin <m.milyutin@postgrespro.ru>
2016-10-31 20:45:16 +0300

    Transfer some internal functions to compat.c and fix check of postgres version in tests

--- tests/partitioning_test.py
+++ tests/partitioning_test.py
@@ -420,7 +420,7 @@ class PartitioningTests(unittest.TestCase):
         # Check version of postgres server
         # If version < 9.6 skip all tests for parallel queries
         version = node.execute("postgres", "show server_version_num")
-        if version < 90600:
+        if int(version[0][0]) < 90600:
             return
 
         # Prepare test database
