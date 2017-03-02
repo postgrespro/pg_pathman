ffe595e9e958736f0abfdaf3b56ff15a9c62d5ed
Dmitry Ivanov <ivadmi5@gmail.com>
2017-03-02 19:31:24 +0300

    temporarily disabled EXPAND_RTE_HOOK & RELATION_TAGS

--- src/include/compat/debug_compat_features.h
+++ src/include/compat/debug_compat_features.h
@@ -12,8 +12,8 @@
 #define ENABLE_PGPRO_PATCHES
 
 /* PgPro exclusive features */
-#define ENABLE_EXPAND_RTE_HOOK
-#define ENABLE_RELATION_TAGS
+//#define ENABLE_EXPAND_RTE_HOOK
+//#define ENABLE_RELATION_TAGS
 #define ENABLE_PATHMAN_AWARE_COPY_WIN32
 
 /* Hacks for vanilla */
