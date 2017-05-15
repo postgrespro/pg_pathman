0c5323da0109e43a1a52d87b618d19e55086ad7e
Dmitry Ivanov <ivadmi5@gmail.com>
2017-05-15 18:31:24 +0300

    bump version number (1.3.2)

--- src/init.h
+++ src/init.h
@@ -99,7 +99,7 @@ extern PathmanInitState 	pg_pathman_init_state;
 #define LOWEST_COMPATIBLE_FRONT		0x010300
 
 /* Current version on native C library (0xAA_BB_CC) */
-#define CURRENT_LIB_VERSION			0x010300
+#define CURRENT_LIB_VERSION			0x010302
 
 
 void *pathman_cache_search_relid(HTAB *cache_table,
