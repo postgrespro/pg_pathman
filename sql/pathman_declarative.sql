ca9f9ebe86281b4443ce2e30eba48ad85a831728
Ildus K <i.kurbangaliev@gmail.com>
2017-06-23 18:37:32 +0300

    Add tests for ATTACH and DETACH PARTITION commands

--- sql/pathman_declarative.sql
+++ sql/pathman_declarative.sql
@@ -15,14 +15,14 @@ SELECT g FROM generate_series('2015-01-01', '2015-04-30', '1 day'::interval) AS 
 SELECT pathman.create_range_partitions('test.range_rel', 'dt',
 	'2015-01-01'::DATE, '1 month'::INTERVAL);
 
-SELECT * FROM pathman_partition_list;
-CREATE TABLE test.r2 LIKE (test.range_rel);
+SELECT * FROM pathman.pathman_partition_list;
+CREATE TABLE test.r2 (LIKE test.range_rel);
 ALTER TABLE test.range_rel ATTACH PARTITION test.r2
 	FOR VALUES FROM ('2015-05-01') TO ('2015-06-01');
-SELECT * FROM pathman_partition_list;
+SELECT * FROM pathman.pathman_partition_list;
 \d+ test.r2;
 ALTER TABLE test.range_rel DETACH PARTITION test.r2;
-SELECT * FROM pathman_partition_list;
+SELECT * FROM pathman.pathman_partition_list;
 \d+ test.r2;
 
 DROP SCHEMA test CASCADE;
