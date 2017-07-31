efa1b4867a80cf97940ed1995044c51644f699ac
Ildus K <i.kurbangaliev@gmail.com>
2017-07-31 19:10:49 +0300

    Start fixing update node for subpartitions (WIP)

--- sql/pathman_subpartitions.sql
+++ sql/pathman_subpartitions.sql
@@ -3,8 +3,6 @@
 CREATE EXTENSION pg_pathman;
 CREATE SCHEMA subpartitions;
 
-
-
 /* Create two level partitioning structure */
 CREATE TABLE subpartitions.abc(a INTEGER NOT NULL, b INTEGER NOT NULL);
 INSERT INTO subpartitions.abc SELECT i, i FROM generate_series(1, 200, 20) as i;
@@ -28,75 +26,45 @@ EXPLAIN (COSTS OFF) SELECT * FROM subpartitions.abc WHERE b  = 215;
 EXPLAIN (COSTS OFF) SELECT * FROM subpartitions.abc WHERE a  = 215 AND b  = 215;
 EXPLAIN (COSTS OFF) SELECT * FROM subpartitions.abc WHERE a >= 210 AND b >= 210;
 
-
-
-/* Multilevel partitioning with update triggers */
-CREATE OR REPLACE FUNCTION subpartitions.partitions_tree(rel REGCLASS)
-RETURNS SETOF REGCLASS AS
+/* Multilevel partitioning with updates */
+CREATE OR REPLACE FUNCTION subpartitions.partitions_tree(
+	rel REGCLASS,
+	level TEXT DEFAULT ' '
+)
+RETURNS SETOF TEXT AS
 $$
 DECLARE
-	partition		REGCLASS;
-	subpartition	REGCLASS;
+	partition REGCLASS;
+	subpartition TEXT;
 BEGIN
 	IF rel IS NULL THEN
 		RETURN;
 	END IF;
 
-	RETURN NEXT rel;
+	RETURN NEXT rel::TEXT;
 
 	FOR partition IN (SELECT l.partition FROM pathman_partition_list l WHERE parent = rel)
 	LOOP
-		FOR subpartition IN (SELECT subpartitions.partitions_tree(partition))
+		FOR subpartition IN (SELECT subpartitions.partitions_tree(partition, level || ' '))
 		LOOP
-			RETURN NEXT subpartition;
+			RETURN NEXT level || subpartition::TEXT;
 		END LOOP;
 	END LOOP;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION subpartitions.get_triggers(rel REGCLASS)
-RETURNS SETOF TEXT AS
-$$
-DECLARE
-	def TEXT;
-BEGIN
-	FOR def IN (SELECT pg_get_triggerdef(oid) FROM pg_trigger WHERE tgrelid = rel)
-	LOOP
-		RETURN NEXT def;
-	END LOOP;
-
-	RETURN;
-END;
-$$ LANGUAGE plpgsql;
-
-SELECT create_update_triggers('subpartitions.abc_1');	/* Cannot perform on partition */
-SELECT create_update_triggers('subpartitions.abc');		/* Can perform on parent */
-SELECT p, subpartitions.get_triggers(p)
-FROM subpartitions.partitions_tree('subpartitions.abc') as p
-ORDER BY p;
-
 SELECT append_range_partition('subpartitions.abc', 'subpartitions.abc_4');
 SELECT create_hash_partitions('subpartitions.abc_4', 'b', 2);
-SELECT p, subpartitions.get_triggers(p)
-FROM subpartitions.partitions_tree('subpartitions.abc_4') as p
-ORDER BY p;
-
-SELECT drop_triggers('subpartitions.abc_1');	/* Cannot perform on partition */
-SELECT drop_triggers('subpartitions.abc');		/* Can perform on parent */
-SELECT p, subpartitions.get_triggers(p)
-FROM subpartitions.partitions_tree('subpartitions.abc') as p
-ORDER BY p;
-
+SELECT subpartitions.partitions_tree('subpartitions.abc');
 DROP TABLE subpartitions.abc CASCADE;
 
-
-
-/* Test that update trigger works correctly */
+\q
+/* Test that update works correctly */
+SET pg_pathman.enable_partitionupdate=on;
 CREATE TABLE subpartitions.abc(a INTEGER NOT NULL, b INTEGER NOT NULL);
 SELECT create_range_partitions('subpartitions.abc', 'a', 0, 100, 2);
 SELECT create_range_partitions('subpartitions.abc_1', 'b', 0, 50, 2);
 SELECT create_range_partitions('subpartitions.abc_2', 'b', 0, 50, 2);
-SELECT create_update_triggers('subpartitions.abc');
 
 INSERT INTO subpartitions.abc VALUES (25, 25);
 SELECT tableoid::regclass, * FROM subpartitions.abc;	/* Should be in subpartitions.abc_1_1 */
@@ -111,8 +79,5 @@ UPDATE subpartitions.abc SET b = 125 WHERE a = 125 and b = 75;
 SELECT tableoid::regclass, * FROM subpartitions.abc;	/* Should create subpartitions.abc_2_3 */
 
 DROP TABLE subpartitions.abc CASCADE;
-
-
-
 DROP SCHEMA subpartitions CASCADE;
 DROP EXTENSION pg_pathman;
