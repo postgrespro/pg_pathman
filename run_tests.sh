1608d8d3ff1bc77cb4412b3b6acb16c83c170879
Dmitry Ivanov <ivadmi5@gmail.com>
2018-07-05 12:34:23 +0300

    small refactoring in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -6,32 +6,22 @@
 #	* cmocka-based tests
 # Copyright (c) 2017, Postgres Professional
 
-set -eux
+set -ux
 
 echo CHECK_CODE=$CHECK_CODE
+echo PG_VERSION=$(pg_config --version)
 
 status=0
+
+# change relevant core dump settings
+CORE_DIR=/tmp/cores
+ulimit -c unlimited -S
+mkdir "$CORE_DIR"
+echo "$CORE_DIR/%e-%s-%p.core" | sudo tee /proc/sys/kernel/core_pattern
 
 # perform code analysis if necessary
 if [ "$CHECK_CODE" = "clang" ]; then
     scan-build --status-bugs make USE_PGXS=1 || status=$?
-    exit $status
-
-elif [ "$CHECK_CODE" = "cppcheck" ]; then
-    cppcheck \
-		--template "{file} ({line}): {severity} ({id}): {message}" \
-        --enable=warning,portability,performance \
-        --suppress=redundantAssignment \
-        --suppress=uselessAssignmentPtrArg \
-		--suppress=literalWithCharPtrCompare \
-        --suppress=incorrectStringBooleanError \
-        --std=c89 src/*.c src/include/*.h 2> cppcheck.log
-
-    if [ -s cppcheck.log ]; then
-        cat cppcheck.log
-        status=1 # error
-    fi
-
     exit $status
 fi
 
@@ -42,28 +32,20 @@ source env/bin/activate
 pip install testgres
 pip freeze | grep testgres
 
-# don't forget to "make clean"
-make USE_PGXS=1 clean
-
 # initialize database
 initdb
 
 # build pg_pathman (using PG_CPPFLAGS and SHLIB_LINK for gcov)
+set -e
+make USE_PGXS=1 clean
 make USE_PGXS=1 PG_CPPFLAGS="-coverage" SHLIB_LINK="-coverage"
 make USE_PGXS=1 install
-
-# check build
-status=$?
-if [ $status -ne 0 ]; then exit $status; fi
+set +e
 
 # add pg_pathman to shared_preload_libraries and restart cluster 'test'
 echo "shared_preload_libraries = 'pg_pathman'" >> $PGDATA/postgresql.conf
 echo "port = 55435" >> $PGDATA/postgresql.conf
-pg_ctl start -l /tmp/postgres.log -w
-
-# check startup
-status=$?
-if [ $status -ne 0 ]; then cat /tmp/postgres.log; fi
+pg_ctl start -l /tmp/postgres.log -w || cat /tmp/postgres.log
 
 # run regression tests
 export PG_REGRESS_DIFF_OPTS="-w -U3" # for alpine's diff (BusyBox)
@@ -72,17 +54,21 @@ PGPORT=55435 make USE_PGXS=1 installcheck || status=$?
 # show diff if it exists
 if test -f regression.diffs; then cat regression.diffs; fi
 
-set +u
+# list cores and exit if we failed
+ls "$CORE_DIR"
+if [ $status -ne 0 ]; then exit $status; fi
 
 # run python tests
+set +u
 make USE_PGXS=1 python_tests || status=$?
-if [ $status -ne 0 ]; then exit $status; fi
-
 set -u
 
-# run cmocka tests (using CFLAGS_SL for gcov)
-make USE_PGXS=1 PG_CPPFLAGS="-coverage" cmocka_tests || status=$?
+# list cores and exit if we failed
+ls "$CORE_DIR"
 if [ $status -ne 0 ]; then exit $status; fi
+
+# run cmocka tests (using CFLAGS_SL for gcov)
+make USE_PGXS=1 PG_CPPFLAGS="-coverage" cmocka_tests || exit $?
 
 # remove useless gcov files
 rm -f tests/cmocka/*.gcno
@@ -92,6 +78,8 @@ rm -f tests/cmocka/*.gcda
 gcov src/*.c src/compat/*.c src/include/*.h src/include/compat/*.h
 
 # send coverage stats to Coveralls
+set +u
 bash <(curl -s https://codecov.io/bash)
+set -u
 
 exit $status
