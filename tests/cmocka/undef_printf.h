6f51eb4f7b2414ae0307037ae94d6327c1e97385
Arseny Sher <sher-ars@yandex.ru>
2019-11-19 18:26:21 +0300

    Forgot undef file.

---
+++ tests/cmocka/undef_printf.h
@@ -0,0 +1,24 @@
+#ifdef vsnprintf
+#undef vsnprintf
+#endif
+#ifdef snprintf
+#undef snprintf
+#endif
+#ifdef vsprintf
+#undef vsprintf
+#endif
+#ifdef sprintf
+#undef sprintf
+#endif
+#ifdef vfprintf
+#undef vfprintf
+#endif
+#ifdef fprintf
+#undef fprintf
+#endif
+#ifdef vprintf
+#undef vprintf
+#endif
+#ifdef printf
+#undef printf
+#endif
