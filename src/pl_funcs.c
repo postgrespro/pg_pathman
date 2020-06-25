2cdb2bda31bca10c8f643e36cc6d76f3dd64163e
Andrey Borodin <amborodin@acm.org>
2020-06-25 14:31:22 +0500

    Avoid segfaults on 1.5-upgraded installations before 10e6c71

--- src/pl_funcs.c
+++ src/pl_funcs.c
@@ -751,8 +751,8 @@ add_to_pathman_config(PG_FUNCTION_ARGS)
 	uint32				children_count;
 
 	Relation			pathman_config;
-	Datum				values[Natts_pathman_config];
-	bool				isnull[Natts_pathman_config];
+	Datum				values[Natts_pathman_config_historic];
+	bool				isnull[Natts_pathman_config_historic];
 	HeapTuple			htup;
 
 	Oid					expr_type;
@@ -850,6 +850,14 @@ add_to_pathman_config(PG_FUNCTION_ARGS)
 
 	values[Anum_pathman_config_expr - 1]		= CStringGetTextDatum(expression);
 	isnull[Anum_pathman_config_expr - 1]		= false;
+
+	/*
+	 * In case of 1.5 update before 10e6c71 there is acutlly 5 attributes in
+	 * pathman_config description (inclusing cooked expression). To avoid
+	 * potential problems we allocate 5th attribute and initialize it with null.
+	 */
+	values[Natts_pathman_config_historic - 1]	= (Datum) 0;
+	isnull[Natts_pathman_config_historic - 1]	= true;
 
 	/* Insert new row into PATHMAN_CONFIG */
 	pathman_config = heap_open(get_pathman_config_relid(false), RowExclusiveLock);
